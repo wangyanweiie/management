b998e6b1188e581e2c913a1709b78645b03d71b3
wangyanweiie <740484522@qq.com>
2021-09-16 08:17:49 +0800

    抽离main.js中的axios请求

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
-import axios from 'axios'
+import router from './router/index'             //导入vue-router
+import store from './store/index'               //导入vuex的实例
+import request from 'network/request'           //导入axios请求实例
 import './assets/css/global.css'                //引入全局样式
 import ElementUI from 'element-ui';             //完整引入ElementUI库
 import 'element-ui/lib/theme-chalk/index.css';  //完整引入ElementUI样式
@@ -13,33 +13,14 @@ import 'quill/dist/quill.core.css'              //导入文本编辑器的样式
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 
-import NProgress from 'nprogress'               //导入进度条组件
 import 'nprogress/nprogress.css'                //导入进度条组件样式
 
-Vue.use(ElementUI);     //使用ElementUI库
-Vue.use(ZkTable)        //使用ZkTable组件
-Vue.use(VueQuillEditor) //使用文本编辑器组件
+Vue.use(ElementUI);             //使用ElementUI库
+Vue.use(ZkTable)                //使用ZkTable组件
+Vue.use(VueQuillEditor)         //使用文本编辑器组件
+Vue.prototype.$http = request   //axios实例挂载到Vue原型上
 Vue.config.productionTip = false
-//***********************************************************************
-//1.配置请求的根路径
-axios.defaults.baseURL = 'http://www.ysqorz.top:8888/api/private/v1'
 
-//2.给axios设置拦截器
-//请求拦截器
-//为请求头对象添加token验证的Authorization字段保证拥有获取数据的权限;
-axios.interceptors.request.use(config=>{
-  config.headers.Authorization = window.sessionStorage.getItem('token');
-  NProgress.start();    //显示加载进度条效果
-  return config;
-})
-//响应拦截器
-axios.interceptors.response.use(result=>{
-  NProgress.done();     //隐藏加载进度条效果
-  return result;
-})
-//3.将axios请求挂载到Vue原型上
-Vue.prototype.$http = axios
-//**********************************************************************     
 //声明vue实例
 new Vue({
   router,
---
+++ src/network/request.js
@@ -0,0 +1,28 @@
+import axios from 'axios'           //导入axios框架
+import NProgress from 'nprogress'   //导入加载进度条组件
+
+  //1.创建axios的实例
+  const instance = axios.create({
+    baseURL: 'http://www.ysqorz.top:8888/api/private/v1',
+    timeout: 5000
+  })
+
+  //2.拦截器 (config是需要传入的参数)
+  //请求拦截器: 为请求头对象添加token验证的Authorization字段保证拥有获取数据的权限;
+  instance.interceptors.request.use(config => {
+    config.headers.Authorization = window.sessionStorage.getItem('token');
+    NProgress.start();    //显示加载进度条效果
+    return config
+  }, err => {
+    console.log(err);
+  })
+  //响应拦截器
+  instance.interceptors.response.use(result => {
+    NProgress.done();     //隐藏加载进度条效果
+    return result;
+  }, err => {
+    console.log(err.message);
+  })
+
+  //3.导出axios请求实例到main.js中
+  export default instance
