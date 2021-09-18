2a351dbdafa02d52b99ead49fab6f3a3778dba17
wangyanweiie <740484522@qq.com>
2021-09-18 10:27:04 +0800

    修改配置信息

--- src/main.js
+++ src/main.js
@@ -13,11 +13,11 @@ import 'quill/dist/quill.core.css'              //导入文本编辑器的样式
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 
-Vue.use(ElementUI);             //使用ElementUI库
-Vue.use(ZkTable)                //使用ZkTable组件
-Vue.use(VueQuillEditor)         //使用文本编辑器组件
-Vue.prototype.$http = request   //axios实例挂载到Vue原型上
-Vue.config.productionTip = false
+Vue.use(ElementUI);               //使用ElementUI库
+Vue.use(ZkTable)                  //使用ZkTable组件
+Vue.use(VueQuillEditor)           //使用文本编辑器组件
+Vue.prototype.$http = request     //axios实例挂载到Vue原型上
+Vue.config.productionTip = false  //阻止启动生产消息提示
 
 //声明vue实例
 new Vue({
